1b74edb3ac693e923ea798966197b1c28214438a
Sergio <sehx.karoka@gmail.com>
2025-04-24 11:49:39 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,4 +4,10 @@ import React from 'react';
 import App from './App.tsx'
 import './index.css'
 
-createRoot(document.getElementById("root")!).render(<React.StrictMode><App /></React.StrictMode>);
+createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <BrowserRouter> {/* 👈 Wrapping ici */}
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+)
