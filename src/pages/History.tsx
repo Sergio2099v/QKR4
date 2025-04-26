30f9c65c6665db29bfe0d6399a64d6ada62033f4
Sergio <sehx.karoka@gmail.com>
2025-04-26 15:08:30 +0300

    Update History.tsx

--- src/pages/History.tsx
+++ src/pages/History.tsx
@@ -116,7 +116,7 @@ export default function History() {
       
       // En-tête
       doc.setFontSize(20);
-      doc.setFont(undefined, 'bold', 'italic');
+      doc.setFont(undefined, 'bolditalic');
       doc.setTextColor(118, 2, 10);
       doc.text('RESULTATS DU QUIZ', 20, 20);
 
